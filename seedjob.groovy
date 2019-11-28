fb9711e2bf45666b5354eca38d304d98219d01b0
Simpson, Chris <chris.simpson@capgemini.com>
2019-11-28 14:06:07 +0000

    remove git repo

--- seedjob.groovy
+++ seedjob.groovy
@@ -2,7 +2,6 @@ pipelineJob('example') {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/jenkinsci/job-dsl-plugin.git')
             }
         }
     }
