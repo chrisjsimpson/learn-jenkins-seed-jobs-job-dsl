2ee2c06ed8fd809e33ac0386f8ab8a1f7c82430a
Simpson, Chris <chris.simpson@capgemini.com>
2019-11-28 15:47:30 +0000

    http origin

--- seedjob.groovy
+++ seedjob.groovy
@@ -5,7 +5,7 @@ pipelineJob('OBP/OBP-build') {
     definition {
         cpsScm {
             scm {
-              git('git@github.com:OpenBankProject/OBP-CLI.git')
+              git('https://github.com/OpenBankProject/OBP-CLI.git')
             }
         }
     }
